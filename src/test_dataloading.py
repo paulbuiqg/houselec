f67a1ab8f2e1fe893faacf4e3e649b15f9bf3478
paul <pbuiquang@vegetalsignals.com>
2024-05-14 17:38:23 +0200

    Add unit tests

---
+++ src/test_dataloading.py
@@ -0,0 +1,37 @@
+"""Unit testing for the preprocessing script."""
+
+import random
+
+import pytest
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+import dataloading
+import preprocessing
+
+
+@pytest.fixture
+def dataset() -> Dataset:
+    data = preprocessing.fetch_data()
+    data, feats, target = preprocessing.prepare_data(data)
+    return dataloading.ElectricTimeSeries(data, feats, target)
+
+
+@pytest.fixture
+def dataloader(dataset: Dataset) -> DataLoader:
+    return DataLoader(dataset, batch_size=128, shuffle=False,
+                      collate_fn=dataloading.collate_fn)
+
+
+def test_dataset(dataset: Dataset):
+    i = random.randrange(len(dataset))
+    X, y = dataset.__getitem__(i)
+    X_size = torch.Size([dataloading.N_INPUT_HOUR, len(dataset.feats)])
+    y_size = torch.Size([1])
+    assert X.size() == X_size and y.size() == y_size
+
+
+def test_dataloader(dataloader: DataLoader):
+    for i, _ in enumerate(dataloader):
+        continue
+    assert i == len(dataloader) - 1
